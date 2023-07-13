812c95396d33d9df39cbd53cc125a15757558177
t-d-h <tdhoan526@gmail.com>
2023-07-13 18:08:25 +0700

    add k8s manifests

--- jenkins/docker-build.groovy
+++ jenkins/docker-build.groovy
@@ -9,17 +9,26 @@ stage('Code checking') {
             error("Error when executing the code, please see the above logs")
         }
         // connection test
-        sh(script: "./testing-scripts/test.sh")
-        def RunCheck = sh(script: "start-golang.sh", returnStatus: true)
+        sh(script: "/home/jenkins/testing-scripts/start-golang.sh")
+        def RunCheck = sh(script: "/home/jenkins/testing-scripts/curl-test.sh > /dev/null", returnStatus: true)
         if (RunCheck !=0 ) {
             error("Application crashed when testing")
         }
+        println("========End of code checking stage========")
     }
 }
 
 
-stage('Performance check') {
+stage('Building-pushing container image') {
     node("k1") {
-
+        git branch: 'main', url: "https://github.com/t-d-h/project-3.git"
+        def getVersion = sh(script: "cat golang-http/info", returnStdout: true)
+        def buildContainerImage = sh(script: "docker build -t ${getVersion} golang-http/")
+        def pushIMGtoRegistry = sh(script: "docker push ${getVersion}")
+        println("========End of container building stage========")
     }
+}
+
+stage('Confirm to deploy') {
+    input message: "All test passed, do you want to deploy it on dev?", ok: 'OK'
 }
--- testing-scripts/curl-test.sh
+++ testing-scripts/curl-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in `seq 1 20`; 
-do curl localhost:8090; 
+do curl localhost:8090 > /dev/null; 
 done
